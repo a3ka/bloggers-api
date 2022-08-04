fe62811257f83973a14c7e7e4103aaccf3623e56
Oleksandr Kalynovskyy <alex.kurz@protonmail.com>
2022-08-04 21:10:22 +0300

    heroku_test#2

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.use(cors())
 app.use(bodyParser())
 
 // const port = process.env.PORT || 5000
-const port = process.env.PORT 
+const port = process.env.PORT
 
 app.use('/bloggers', bloggersRouter)
 app.use('/posts', postsRouter)
