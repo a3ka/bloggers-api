c56231160bc33aff75b38b88eae5f16d177a7e6b
Oleksandr Kalynovskyy <alex.kurz@protonmail.com>
2022-08-04 22:34:28 +0300

    heroku_test#5

--- src/routers/auth-router.ts
+++ src/routers/auth-router.ts
@@ -9,9 +9,9 @@ import {jwtService} from "../application/jwt-service";
 export const authRouter = Router({})
 
 authRouter.post('/login',
-    fieldsValidationMiddleware.loginValidation,
-    fieldsValidationMiddleware.passwordValidation,
-    inputValidationMiddleware,
+    // fieldsValidationMiddleware.loginValidation,
+    // fieldsValidationMiddleware.passwordValidation,
+    // inputValidationMiddleware,
 
     async (req: Request, res: Response) => {
         const user = await usersService.checkCredentials(req.body.login, req.body.password)
