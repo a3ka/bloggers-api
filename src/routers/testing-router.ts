01fbf35244ae36fe1a0b77d63a922caf4c9a7e48
Oleksandr Kalynovskyy <alex.kurz@protonmail.com>
2022-08-14 15:22:37 +0300

    release Blogger API 5.0

--- src/routers/testing-router.ts
+++ src/routers/testing-router.ts
@@ -5,19 +5,15 @@ import {bloggersRepository} from "../repositories/bloggers-db-repository";
 import {commentsRepository} from "../repositories/comments-db-repository";
 
 
-
 export const testingRouter = Router({})
 
 testingRouter.delete('/all-data',
     async (req: Request, res: Response) => {
-
-            const delAllPost = await postsRepository.deleteAllPost()
-
-            const delAllUsers = await usersRepository.deleteAllUsers()
-
-            const delAllBloggers = await bloggersRepository.deleteAllBloggers()
-            const delAllComments = await commentsRepository.deleteAllComments()
-
+        await postsRepository.deleteAllPost()
+        await usersRepository.deleteAllUsers()
+        await bloggersRepository.deleteAllBloggers()
+        await commentsRepository.deleteAllComments()
+        
         res.sendStatus(204)
     }
 )
